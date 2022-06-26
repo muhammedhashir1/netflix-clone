3541bc6db1a2b7865eccf568548ab36105f55a27
Muhammed Hashir <hashiraachi818@gmail.com>
2022-06-27 00:06:24 +0530

    font size fixed and banner set

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -10,7 +10,7 @@ function Banner() {
     const [movie, setMovie] = useState();
 
     useEffect(() => {
-      axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+      axios.get(`trending/all/day?api_key=${API_KEY}&language=en-US`).then((response)=>{
         console.log(response.data.results[0])
         //setMovie(response.data.results[3],[4])
         const movie_num = Math.floor(Math.random() * response.data.results.length);
@@ -28,14 +28,14 @@ function Banner() {
        className='banner'>
           
           <div className='content'>
-              <h1 className='banner__title'>{movie ? movie.title : ""}</h1>
+              <h1 className='banner__title'>{movie ? movie.original_title : ""}</h1>
               <div className='banner_buttons'>
                   <button className='button' >Play</button>
                   <button className='button'>My Playlist</button>
               </div>
               {/* <h5 >Date: {movie ? movie.release_date :""}</h5> */}
-              <h5 className='banner__language'>Language: {movie ? movie.original_language :""}</h5>
-              <h5 className='banner__rating'>Rating: {movie ? movie.vote_average :""}</h5>
+              {/* <h5 className='banner__language'>Language: {movie ? movie.original_language :""}</h5>
+              <h5 className='banner__rating'>Rating: {movie ? movie.vote_average :""}</h5> */}
               <h1 className='banner__description'>{movie ? movie.overview : ""}</h1>
           </div>
           <div className="fade_bottom"></div>
--- src/urls.js
+++ src/urls.js
@@ -1,6 +1,6 @@
 import { API_KEY } from "./constants/constants"
 
-export const Trending = `trending/all/week?api_key=${API_KEY}&language=en-US'`
+export const Trending = `trending/all/day?api_key=${API_KEY}&language=en-US'`
 export const orginals = `discover/tv?api_key=${API_KEY}&with_networks=213`
 export const action = `discover/movie?api_key=${API_KEY}&with_genres=28`
 export const horror = `discover/movie?api_key=${API_KEY}&with_genres=27`
