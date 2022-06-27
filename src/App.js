a37f1db8031ec8a0b254864fee40c95757fb76a1
Muhammed Hashir <hashiraachi818@gmail.com>
2022-06-27 11:22:52 +0530

    footer set

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from "react";
-import NavBar from "./Components/NavBar/NavBar";
 import "./App.css"
+import NavBar from "./Components/NavBar/NavBar";
 import Banner from "./Components/Banner/Banner";
 import RowPost from "./Components/RowPost/RowPost";
+import Footer from "./Components/Footer/Footer";
 import {Trending,orginals,action,horror,tv}  from './urls'
+
 
 function App() {
   return (
     <div>
+
     <NavBar/>
+    
     <Banner/>
+    
     <RowPost url={Trending} title='Trending Now'  />
     <RowPost url={orginals} title='Netflix Orginals' isSmall />
     <RowPost url={action} title='Action' isSmall/>
     <RowPost url={horror} title='Horror' isSmall/>
     <RowPost url={tv} title='TV Shows' isSmall/>
+
+    <Footer/>
     </div>
   );
 }
